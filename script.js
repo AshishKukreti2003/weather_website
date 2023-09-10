80b8a508a98dcdb123ec7b1eb800b34a753f3465
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-09-10 21:17:16 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '36e65ae8f5msh6b2c76df7c2faffp1ad728jsn99229d532abd',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "36e65ae8f5msh6b2c76df7c2faffp1ad728jsn99229d532abd",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
-async function fetchWeatherData() {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-}
+const getWeather = (city) => {
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
 
-fetchWeatherData(); // Call the async function to initiate the request
+    //   cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      max_temp.innerHTML = response.max_temp;
+      min_temp.innerHTML = response.min_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather("Delhi");
